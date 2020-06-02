fbb2e4e034b6e24deab28b40384ec63e9ae12f99
xujiangqiao <xujiangqiao@bigo.sg>
2020-06-02 21:20:19 +0800

    add subscriptionName to checkpoint

--- src/main/java/org/apache/flink/streaming/connectors/pulsar/FlinkPulsarSource.java
+++ src/main/java/org/apache/flink/streaming/connectors/pulsar/FlinkPulsarSource.java
@@ -352,10 +352,10 @@ public class FlinkPulsarSource<T>
 
         ownedTopicStarts = new HashMap<>();
         Set<String> allTopics = metadataReader.discoverTopicChanges();
+
         log.info("Discovered topics : {}", allTopics);
 
         boolean canUsingRestoreState = ( startupMode != StartupMode.EXTERNAL_SUBSCRIPTION ) || unionSubEqualExternalSub;
-
         if (restoredState != null && canUsingRestoreState) {
             allTopics.stream()
                     .filter(k -> !restoredState.containsKey(k))

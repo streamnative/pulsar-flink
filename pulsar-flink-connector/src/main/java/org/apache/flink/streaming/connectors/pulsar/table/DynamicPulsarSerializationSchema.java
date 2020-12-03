ae8be3e91cbf6e8ba285cd20373ae9bf218e8b00
Jianyun Zhao <yihy8023@gmail.com>
2020-12-03 15:17:56 +0800

    fix formatType value is empty for class DynamicPulsarSerializationSchema.

--- pulsar-flink-connector/src/main/java/org/apache/flink/streaming/connectors/pulsar/table/DynamicPulsarSerializationSchema.java
+++ pulsar-flink-connector/src/main/java/org/apache/flink/streaming/connectors/pulsar/table/DynamicPulsarSerializationSchema.java
@@ -200,7 +200,6 @@ class DynamicPulsarSerializationSchema
         }
     }
 
-
     // --------------------------------------------------------------------------------------------
 
     interface MetadataConverter extends Serializable {

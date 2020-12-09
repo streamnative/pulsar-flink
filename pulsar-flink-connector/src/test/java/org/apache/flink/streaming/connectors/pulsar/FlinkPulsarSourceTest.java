67329a8cf296bd8fd5cd29e4c91ed42f2dec85e5
Jianyun Zhao <yihy8023@gmail.com>
2020-12-09 13:53:12 +0800

    update pulsar version to 2.7.0 (#206)

--- pulsar-flink-connector/src/test/java/org/apache/flink/streaming/connectors/pulsar/FlinkPulsarSourceTest.java
+++ pulsar-flink-connector/src/test/java/org/apache/flink/streaming/connectors/pulsar/FlinkPulsarSourceTest.java
@@ -509,7 +509,8 @@ public class FlinkPulsarSourceTest extends TestLogger {
         private final RuntimeException failureCause;
 
         public FailingPartitionDiscoverer(RuntimeException failureCause) throws PulsarClientException {
-            super("", new ClientConfigurationData(), "", Collections.singletonMap("topic", "foo"), 0, 1);
+            super("http://localhost:8080", new ClientConfigurationData(), "", Collections.singletonMap("topic", "foo"),
+                    0, 1);
             this.failureCause = failureCause;
         }
 
@@ -539,7 +540,8 @@ public class FlinkPulsarSourceTest extends TestLogger {
         private static Set<TopicRange> allPartitions = Sets.newHashSet(new TopicRange("foo"));
 
         public DummyPartitionDiscoverer() throws PulsarClientException {
-            super("", new ClientConfigurationData(), "", Collections.singletonMap("topic", "foo"), 0, 1);
+            super("http://localhost:8080", new ClientConfigurationData(), "", Collections.singletonMap("topic", "foo"),
+                    0, 1);
         }
 
         @Override

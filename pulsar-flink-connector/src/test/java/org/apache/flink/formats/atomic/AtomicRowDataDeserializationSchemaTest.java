6ce2f2ef5a763ab0916c0fc4cc9700d6bc6170f7
Jianyun Zhao <yihy8023@gmail.com>
2020-12-16 21:05:36 +0800

    fix code style

--- pulsar-flink-connector/src/test/java/org/apache/flink/formats/atomic/AtomicRowDataDeserializationSchemaTest.java
+++ pulsar-flink-connector/src/test/java/org/apache/flink/formats/atomic/AtomicRowDataDeserializationSchemaTest.java
@@ -1,9 +1,9 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,6 +33,9 @@ import java.time.LocalTime;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * test for AtomicRowDataDeserializationSchema.
+ */
 @RunWith(Parameterized.class)
 public class AtomicRowDataDeserializationSchemaTest extends TestLogger {
 
@@ -119,5 +122,4 @@ public class AtomicRowDataDeserializationSchemaTest extends TestLogger {
         final Object value = schema.decode(encode);
         assertEquals(expectedValue, value);
     }
-
 }

71e5bcdfaeb3eea786bf85bcf431c77a3fb3b1e4
shibd <wudixiaolongbao@gmail.com>
2021-06-21 01:00:50 +0800

    fixbug #237: NotSerializableException in submitting flink job

---
+++ pulsar-flink-connector/src/main/java/org/apache/flink/streaming/util/serialization/SchemaInfoWrapper.java
@@ -0,0 +1,35 @@
+package org.apache.flink.streaming.util.serialization;
+
+import org.apache.pulsar.common.schema.SchemaInfo;
+import org.apache.pulsar.common.schema.SchemaType;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * wrapper {@link SchemaInfo}, Resolve that a job cannot be serialized when submitted to flink.
+ * https://github.com/streamnative/pulsar-flink/issues/237
+ */
+public class SchemaInfoWrapper implements Serializable {
+
+    private String name;
+    private byte[] schema;
+    private SchemaType type;
+    private Map<String, String> properties;
+
+    private transient SchemaInfo schemaInfo;
+
+    public SchemaInfoWrapper(SchemaInfo schemaInfo) {
+        this.name = schemaInfo.getName();
+        this.schema = schemaInfo.getSchema();
+        this.type = schemaInfo.getType();
+        this.properties = schemaInfo.getProperties();
+    }
+
+    public SchemaInfo getSchemaInfo() {
+        if (schemaInfo == null) {
+            schemaInfo = new SchemaInfo(name, schema, type, properties);
+        }
+        return schemaInfo;
+    }
+}
